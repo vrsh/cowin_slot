2fb953c09f62ee3594bfcdc3c8b1e29fbf1ccf41
RiteshIAR <ritesh2k08@gmail.com>
2021-05-28 19:07:10 +0530

    added python file

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# cowin_slot
-Python script for cowin alerts
+# Cowin Vaccination Slot ALert Script
+Ultra light Python script for cowin alerts for available slots in current week.
+
+Run this script on any system having python installed.
+To run execute: 
+
+```bash
+python slot_finder.py
+```
+
+This will keep running unless you force quit and will try to fetch info every 15 seconds (default). Frequency can be changed. Read below to know how.
+
+## INPUT: 
+**distrct_id:** district id for your district (default 294 for bengaluru bbmp). Find your district's ID in district_ids.csv file. Districts are sorted statewise and states are sorted alphabetically in this file.
+
+**age_group:** 18 for 18+ and 45 for 45+ (default 18)
+
+**dose:** 1 for dose1 and 2 for dose2 (default 1)
+
+**centers:** names of centers of your choice, comment selected centers and uncomment ```centers=['All']``` (line 49) for selecting all centers in the district
+
+**update_time:** update time in minutes (default 15 seconds)
+
+## OUTPUT: 
+**Case 1:** *when a slot is not available:* 
+nothing (99.99% time for 18+, facepalm)
+
+**Case 2:** *when a slot for selected dose is available for your age group:* 
+a beep sound to alert with details of date of availability, 
+center name, its address, available slots and fee type
+same info is stored in cowin_slots.csv file
+
+*To stop the script interrupt by pressing Ctrl+c*
+
+## Trick: 
+Most hospitals in BBMP open slots after 4.30PM
---
+++ slot_finder.py
@@ -0,0 +1,94 @@
+'''
+A vaccination slot alert system
+Alerts for available slots in current week
+
+Run this script on any system having python installed
+to run execute: python cowin_dist_weekly.py
+This will keep running unless you force quit and will 
+try to fetch info every 5 minute (default)
+frequency can be changed. Read below to know how.
+
+INPUT: 
+distrct_id: district id for your district (default 294 for bengaluru bbmp)
+age_group: 18 for 18+ and 45 for 45+ (default 18)
+dose: 1 for dose1 and 2 for dose2 (default 1)
+centers: names of centers of your choice, comment selected centers and
+uncomment centers=['All'] (line 49) for selecting all centers in the district
+update_time: update time in minutes (default 15 seconds)
+
+OUTPUT: 
+Case 1: when a slot is not available: 
+nothing (99.99% time for 18+, thanks to our planning and execution system)
+
+Case 2: when a slot for dose is available for your age group: 
+a beep sound to alert with details of date of availability, 
+center name, its address, available slots and fee type
+same info is stored in slots.csv file
+
+Author: Ritesh Sharma
+'''
+
+import requests
+import datetime
+import json
+import time
+
+# BBMP is 294
+district_id = 294
+
+# 18 or 45
+age_group = 18
+
+# 1 or 2
+dose = 1
+
+# All or selcted
+# centers = ['AGARA GOVT SCHOOL', 'AGARA UPHC C1', 'APOLLO CLINIC HSR', 
+#             'APOLLO CRADLE - Koramangala', 'APOLLO SPECTRA HOSPITAL -1', 
+#             'Columbia Asia Sarjapur P3', 'Doddakannalli UPHC COVAXIN', 
+#             'Fortis Hospital BG ROAD P3', 'Fortis Lafemme BLOCK 1', 
+#             'HSR CLUB', 'Koramangala UPHC', 'St Johns Hospital', 
+#             'CLOUDNINE BELLANDUR P3']
+centers = ['All']
+
+# in minutes
+update_time = 0.25
+
+slots = open('slots.csv', 'w')
+slots.write('Alert Time,Date,Center,Address,Availability,Fee Type\n')
+
+try:
+    while True:
+        # date = (datetime.date.today()+datetime.timedelta(1)).strftime("%d-%m-%Y")
+        date = datetime.date.today().strftime("%d-%m-%Y")
+        URL = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={district_id}&date={date}"
+        response = requests.get(URL)
+    
+        if response.ok:
+            resp_json = response.json()
+            if resp_json["centers"]:
+                for center in resp_json["centers"]:
+                    for sel_center in centers:
+                        if sel_center=='All' or center["name"] == sel_center:
+                            for session in center["sessions"]:
+                                if session["min_age_limit"] == age_group:
+                                    if session[f"available_capacity_dose{dose}"] > 0:
+                                        print('\n~~~~~~~~~~~~~~~~~\n')
+                                        print(f'Alert Time: {datetime.datetime.now()}')
+                                        print(f'Date: {session["date"]}')
+                                        print(f'Center: {center["name"]}')
+                                        print(f'Address: {center["address"]}')
+                                        print(f'Availability: {session[f"available_capacity_dose{dose}"]}')
+                                        print(f'Fee Type: {center["fee_type"]}')
+                                        print('\a')
+                                        slots.write(f'{datetime.datetime.now()},{session["date"]},{center["name"]},{center["address"]},{session[f"available_capacity_dose{dose}"]},{center["fee_type"]}\n')
+        # print('\nSearch Complete')
+        time.sleep(update_time*60)
+except KeyboardInterrupt:
+    print("\ninterrupted by USER")
+except:
+    print('\nsome exception')
+finally:
+    print("\nSearch Halted")
+    print("Output saved to slots.csv")
+
